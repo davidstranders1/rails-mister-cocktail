b01e325a2b244ad56fbb96ed629a76816534d5e1
David Stranders <david.stranders@gmail.com>
2020-02-13 17:08:19 +0200

    Did some shit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
+
+  root to: "cocktails#index"
 end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts "creating some ingredients"
+
+
+@mint = Ingredient.create(name: "mint leaves")
+@tomato = Ingredient.create(name: "tomato juice")
+@ice = Ingredient.create(name: "ice")
+@lemon = Ingredient.create(name: "lemon")
+@orange = Ingredient.create(name: "orange juice")
+@vodka = Ingredient.create(name: "vodka")
+
+
+
+@moscow = Cocktail.create(name: "Moscow Mule")
+@bloody = Cocktail.create(name: "Bloody Mary")
+@screwdriver = Cocktail.create(name: "Screwdriver")
+@mojito = Cocktail.create(name: "Mojito")
+@daiquiri = Cocktail.create(name: "Daiquiri")
+@tom = Cocktail.create(name: "Tom Collins")
+
+
+Dose.create!(description: "5ml", cocktail: @moscow, ingredient_id: @mint.id)
+Dose.create!(description: "10ml", cocktail: @bloody, ingredient_id: @tomato.id)
+Dose.create!(description: "15ml", cocktail: @screwdriver, ingredient_id: @orange.id)
+Dose.create!(description: "20ml", cocktail: @mojito, ingredient_id: @vodka.id)
+Dose.create!(description: "15ml", cocktail: @daiquiri, ingredient_id: @ice.id)
+Dose.create!(description: "9ml", cocktail: @tom, ingredient_id: @ice.id)
+
+
+
+puts "finished seeding"
